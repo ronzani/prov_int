60a743b6fd19985b163850ef8192b9efbc1e9339
Robson Ronzani <robson.ronzani@palmas.to.gov.br>
2023-08-25 16:25:37 -0300

    primeiro commit

---
+++ prov_int/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from prov_int.users.models import User
+from prov_int.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
